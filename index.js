320b093192890930988e0e0867a5a1c7e7e71563
dl0312 <leegun2003@gmail.com>
2018-05-20 16:41:44 +0900

    First steps with TypeScript

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var name = "Geon", age = 24, gender = "male";
+var sayHi = function (name, age, gender) {
+    console.log("Hello " + name + ", you are " + age + ", you are a " + gender);
+};
+sayHi(name, age, gender);
