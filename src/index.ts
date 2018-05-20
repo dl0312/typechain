4a8a9685c4fffcf0e4ecff82c57028413c9227ea
dl0312 <leegun2003@gmail.com>
2018-05-20 17:23:21 +0900

    Blockchain creating a block

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,28 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-// when you use typescirpt with react, it's more acceptable that use class than interface
+const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
 
-const lynn = new Human("Lynn", 18, "female");
+let blockchain: [Block] = [genesisBlock];
 
-const person = {
-  name: "Dam",
-  age: 23,
-  gender: "female"
-};
-
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${
-    person.gender
-  }`;
-};
-
-console.log(sayHi(lynn));
+console.log(blockchain);
 
 export {};
